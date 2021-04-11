f04df17cba2956b819abb72f187237986b434435
Zac Stringham <zlstringham@gmail.com>
2021-04-11 00:54:48 -0700

    Refactor ssse3 implementation

--- src/specialized/ssse3.rs
+++ src/specialized/ssse3.rs
@@ -1,4 +1,5 @@
 const NMAX: usize = 5536;
+const CHUNK_SIZE: usize = 32;
 
 #[derive(Copy, Clone, Debug)]
 pub struct State {
@@ -44,28 +45,31 @@ impl State {
 }
 
 #[target_feature(enable = "ssse3")]
-unsafe fn update_simd(mut a: u32, mut b: u32, buf: &[u8]) -> (u32, u32) {
+unsafe fn update_simd<'a>(mut a: u32, mut b: u32, buf: &'a [u8]) -> (u32, u32) {
     #[cfg(target_arch = "x86")]
     use core::arch::x86::*;
     #[cfg(target_arch = "x86_64")]
     use core::arch::x86_64::*;
 
-    let v_zeroes = _mm_set1_epi16(0);
+    let v_zeroes = _mm_setzero_si128();
     let v_ones = _mm_set1_epi16(1);
     let v_weights_left = _mm_set_epi8(
         17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32,
     );
     let v_weights_right = _mm_set_epi8(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16);
 
-    let chunks = buf.chunks(NMAX);
-    for chunk in chunks {
+    let add_reduce = |a: &mut u32, b: &mut u32, chunk: &'a [u8]| -> &'a [u8] {
+        if chunk.len() < CHUNK_SIZE {
+            return chunk;
+        }
+
         let v_num_iterate_bytes = (chunk.len() & 0xffff_ffe0) as u32;
-        b += a * v_num_iterate_bytes;
+        *b += *a * v_num_iterate_bytes;
         let mut v_v1 = _mm_setzero_si128();
         let mut v_v2j = _mm_setzero_si128();
         let mut v_v2k = _mm_setzero_si128();
 
-        let inner_chunks = chunk.chunks_exact(32);
+        let inner_chunks = chunk.chunks_exact(CHUNK_SIZE);
         let remainder = inner_chunks.remainder();
         for inner_chunk in inner_chunks {
             let v_p_left = _mm_lddqu_si128((&inner_chunk).as_ptr() as *const __m128i);
@@ -82,25 +86,27 @@ unsafe fn update_simd(mut a: u32, mut b: u32, buf: &[u8]) -> (u32, u32) {
                 _mm_madd_epi16(v_ones, _mm_maddubs_epi16(v_p_right, v_weights_right)),
             );
         }
-
         v_v1 = _mm_add_epi32(v_v1, _mm_shuffle_epi32(v_v1, 177));
         v_v1 = _mm_add_epi32(v_v1, _mm_shuffle_epi32(v_v1, 78));
-        a += _mm_cvtsi128_si32(v_v1) as u32;
+        *a += _mm_cvtsi128_si32(v_v1) as u32;
 
         let mut v_v2 = _mm_add_epi32(v_v2k, _mm_slli_epi32(v_v2j, 5));
         v_v2 = _mm_add_epi32(v_v2, _mm_shuffle_epi32(v_v2, 177));
         v_v2 = _mm_add_epi32(v_v2, _mm_shuffle_epi32(v_v2, 78));
-        b += _mm_cvtsi128_si32(v_v2) as u32;
+        *b += _mm_cvtsi128_si32(v_v2) as u32;
 
-        let updated = if remainder.is_empty() {
-            (a % crate::baseline::BASE, b % crate::baseline::BASE)
-        } else {
-            crate::baseline::update_slow(a, b, remainder)
-        };
-        a = updated.0;
-        b = updated.1;
+        remainder
+    };
+
+    let chunks = buf.chunks_exact(NMAX);
+    let mut remainder = chunks.remainder();
+    for chunk in chunks {
+        add_reduce(&mut a, &mut b, chunk);
+        a %= crate::baseline::BASE;
+        b %= crate::baseline::BASE;
     }
-    (a, b)
+    remainder = add_reduce(&mut a, &mut b, remainder);
+    crate::baseline::update_slow(a, b, remainder)
 }
 
 #[cfg(test)]
